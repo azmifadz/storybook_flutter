6a98eee58761284606e255b0482c2d4639ee58f5
Azmi Fadzlil <azmifadz111@gmail.com>
2022-01-11 15:19:06 +0800

    Test pushing new widget

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,23 +3,19 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "4e436155d544729791eb7183ce28ded9",
-"index.html": "6ca59a581f072147463f1b97308e5eef",
-"/": "6ca59a581f072147463f1b97308e5eef",
-"main.dart.js": "5fcb434b6360c7e05227c52325253ba3",
+  "version.json": "22fecf927129e30f75a9e87f5cb3af1b",
+"index.html": "b8ab5aff2a0ec61b4205fad8f5f2ecf8",
+"/": "b8ab5aff2a0ec61b4205fad8f5f2ecf8",
+"main.dart.js": "e25f8902bd3a9c800e1411f8cac76bee",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "9e5f015cc5cd13d400db4f4d4ba2ef00",
 "assets/AssetManifest.json": "0204befa48b84f73a1f680476bc3ff31",
-"assets/NOTICES": "0c62bb8314d71b9d74bcffff17180ed2",
+"assets/NOTICES": "20913edca3eb2d43f65d52c5db03ea4e",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/packages/storybook_flutter/assets/README.md": "fbaed1ae60d80596d99545ef45d72803",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
-"canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
-"canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
-"canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498"
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
 };
 
 // The application shell files that are downloaded before a service worker can
